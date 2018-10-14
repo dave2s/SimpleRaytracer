52d44447d61422cc780eda8287665ade051e2bd8
Dave2S <xskace12@vutbr.cz>
2018-10-14 23:20:59 +0200

    added camera class with near and far plane attributes

---
+++ Raytracer/Raytracer/Camera.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define NEAR_PLANE 0.1f
+#define FAR_PLANE 100.0f
+
+class Camera
+{
+public:
+	Camera();
+	~Camera();
+
+	/*float near_plane = NEAR_PLANE;
+	float far_plane = FAR_PLANE;*/
+
+private:
+
+};
+
