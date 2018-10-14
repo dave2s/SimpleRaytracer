2b082d3df7746d28869ae2ba561bc514c9c9ad30
Dave2S <xskace12@vutbr.cz>
2018-10-14 23:32:13 +0200

    Added parallel check in getPlaneIntersectionDistance,return zero if paralel. Added check for intesection being negative in raytraceTriangle()

--- Raytracer/Raytracer/RT_Mesh.cpp
+++ Raytracer/Raytracer/RT_Mesh.cpp
@@ -24,6 +24,24 @@ void RT_Mesh::ClearMesh()
 
 }
 
+bool RT_Mesh::raytraceTriangle(glm::vec3 triangle[3],glm::vec3 origin,glm::vec3 ray_dir, Camera camera)
+{//TODO edit to use references or pointers?
+	glm::vec3 n = RT_Mesh::getTriangleNormal(triangle);
+	float d = getDistanceFromOrigin(n, triangle[0]);
+	float hit_distance = getPlaneIntersectionDistance(d, n, origin, ray_dir);
+
+	if (hit_distance <= NEAR_PLANE) {
+		return false; // Triangle is behind the camera OR it's parallel with the ray - both invisible ///TODO too far - fake dust in air
+	}
+
+	glm::vec3 Phit = getPlaneIntersection(origin, hit_distance, ray_dir);
+
+	//paralelni?
+
+
+	return true;
+}
+
 RT_Mesh::~RT_Mesh()
 {
 	ClearMesh();
--- Raytracer/Raytracer/RT_Mesh.h
+++ Raytracer/Raytracer/RT_Mesh.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <GLM\glm.hpp>
+#include "Camera.h"
 class RT_Mesh
 {
 public:
@@ -11,8 +12,12 @@ public:
 
 	static glm::vec3 getTriangleNormal(glm::vec3 vertices[3]){ return glm::normalize(glm::cross(vertices[1] - vertices[0], vertices[2] - vertices[0])); }
 	static float getDistanceFromOrigin(glm::vec3 normal, glm::vec3 vertex) { return glm::dot(normal,vertex); }
-	static float getPlaneIntersectionDistance(float distance_from_origin,glm::vec3 plane_normal,glm::vec3 origin, glm::vec3 ray_direction) { return ((glm::dot(plane_normal,origin)+distance_from_origin) / glm::dot(plane_normal, ray_direction)); }
+	static float getPlaneIntersectionDistance(float distance_from_origin,glm::vec3 plane_normal,glm::vec3 origin, glm::vec3 ray_direction) {float ray_dot_normal = glm::dot(plane_normal, ray_direction);if (ray_dot_normal == 0/*ray&plane parallel + dont divide by zero*/) { return 0;}	else { return ((glm::dot(plane_normal, origin) + distance_from_origin) / glm::dot(plane_normal, ray_direction));}
+	
+	}
 	static glm::vec3 getPlaneIntersection(glm::vec3 origin, float intersection_distance,glm::vec3 ray_direction) { return origin + (intersection_distance*ray_direction) ;}
+	///TODO
+	static bool raytraceTriangle(glm::vec3 triangle[3], glm::vec3 origin, glm::vec3 ray_dir, Camera camera);
 
 	~RT_Mesh();
 
