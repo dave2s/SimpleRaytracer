037544bed004e91b799286cc1f9a09f98f265e1a
xskace12 <xskace12@vutbr.cz>
2018-10-16 16:53:01 +0200

    normalized and flipped direction of the ray, there are hits, but draw is broken somehow

--- Raytracer/Raytracer/Ray.cpp
+++ Raytracer/Raytracer/Ray.cpp
@@ -14,5 +14,6 @@ Ray::~Ray()
 glm::vec3 Ray::calcRayPerspectiveDirection(glm::vec3 origin, float x, float y, int width, int height, float near)
 {
 	//return glm::vec3(glm::vec3((-width/2)+x,(-height/2)+y,near) - origin);
-	return glm::vec3(origin - glm::vec3(x, y, near));
+	//return glm::vec3(origin - glm::vec3(x, y, near));
+	return glm::normalize(glm::vec3(glm::vec3(x, y, near)-origin));
 }
