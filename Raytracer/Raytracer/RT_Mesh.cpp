cd4616e41ccf7fe5fa69b1008d4131f83143ad6d
xskace12 <xskace12@vutbr.cz>
2018-10-16 13:53:27 +0200

    opravena chyba s singlesided boolem...nasrat na reference kdyz jim uz zas nerozumis

--- Raytracer/Raytracer/RT_Mesh.cpp
+++ Raytracer/Raytracer/RT_Mesh.cpp
@@ -10,9 +10,11 @@ RT_Mesh::RT_Mesh()
 
 void RT_Mesh::CreateMesh(float *_vertices, unsigned int *_indices, unsigned int _vertex_count, unsigned int _index_count, bool _singleSided) {
 	index_count = _index_count;
-	vertices = _vertices;
+	vertices = new float[_vertex_count];
+	memcpy(vertices,_vertices,sizeof(float)*_vertex_count);
 	vertex_count = _vertex_count;
-	indices = _indices;
+	indices = new unsigned int[_index_count];
+	memcpy(indices,_indices,sizeof(unsigned int)*_index_count);
 	singleSided = _singleSided;
 }
 
@@ -21,14 +23,14 @@ void RT_Mesh::ClearMesh()
 
 }
 
-bool RT_Mesh::rayHitTriangle(glm::vec3 triangle[3],glm::vec3 origin,glm::vec3 ray_dir, Camera camera, bool& _singleSided)
+bool RT_Mesh::rayHitTriangle(std::vector<glm::vec3> _triangle, glm::vec3 origin, glm::vec3 ray_dir, Camera camera, bool _singleSided)
 {//TODO edit to use references or pointers?
 	//std::cout << "kreslim vertexy: v0: " << glm::to_string(triangle[0]) << " a v1: " << glm::to_string(triangle[1]) << " a v2: " << glm::to_string(triangle[2]) << " \n";
-	glm::vec3 normal = RT_Mesh::getTriangleNormal(triangle);//is normalized
-
-	float d = getDistanceFromOrigin(normal, triangle[0]);
+	glm::vec3 normal = RT_Mesh::getTriangleNormal(_triangle);//is normalized
+	//std::cout <<"normala: "<< glm::to_string(normal) << " vertex 0 = " << glm::to_string(_triangle[0])<<"\n";
+	float d = getDistanceFromOrigin(normal, _triangle[0]);
 	float hit_distance = getPlaneIntersectionDistance(d, normal, origin, ray_dir, _singleSided);
-
+	
 	if (hit_distance <= CAM_NEAR_PLANE/*&&hit_distance==0*/) {
 		return false; // Triangle is behind the camera OR it's parallel with the ray OR it's faced the other way and is single sided - both invisible ///TODO too far - fake dust in air
 	}
@@ -38,11 +40,12 @@ bool RT_Mesh::rayHitTriangle(glm::vec3 triangle[3],glm::vec3 origin,glm::vec3 ra
 		glm::dot(normal, glm::cross(triangle[2] - triangle[1], Phit - triangle[1])) > 0 &&
 		glm::dot(normal, glm::cross(triangle[0] - triangle[2], Phit - triangle[2])) > 0
 		));*/
+
 	//TODO encapsulate
 	///is Phit inside the triangle? test against each edge
-	if (glm::dot(normal, glm::cross(triangle[1] - triangle[0], Phit - triangle[0])) < 0) { return false; }
-	if (glm::dot(normal, glm::cross(triangle[2] - triangle[1], Phit - triangle[1])) < 0) { return false; }
-	if (glm::dot(normal, glm::cross(triangle[0] - triangle[2], Phit - triangle[2])) < 0) { return false; }
+	if (glm::dot(normal, glm::cross(_triangle[1] - _triangle[0], Phit - _triangle[0])) < 0) { return false; }
+	if (glm::dot(normal, glm::cross(_triangle[2] - _triangle[1], Phit - _triangle[1])) < 0) { return false; }
+	if (glm::dot(normal, glm::cross(_triangle[0] - _triangle[2], Phit - _triangle[2])) < 0) { return false; }
 
 
 	return true; //successful hit
--- Raytracer/Raytracer/RT_Mesh.h
+++ Raytracer/Raytracer/RT_Mesh.h
@@ -2,6 +2,7 @@
 #include <GLM\glm.hpp>
 #include "Camera.h"
 #include <iostream>
+#include <vector>
 #define GLM_ENABLE_EXPERIMENTAL
 #include "glm/gtx/string_cast.hpp"
 class RT_Mesh
@@ -16,23 +17,26 @@ public:
 	int getTriangleCount() { return 1;/* return (sizeof(indices)/sizeof(*indices)) / 3; */ }
 
 	//Return triangle by index of the triangle
-	glm::vec3* getTriangle(unsigned int idx) {
+	std::vector<glm::vec3> getTriangle(unsigned int idx) {
 		glm::vec3 v0 = { vertices[indices[0 + 3 * idx] * 3 + 0], vertices[indices[0 + 3 * idx] * 3 + 1], vertices[indices[0 + 3 * idx] * 3 + 2] };
 		glm::vec3 v1 = { vertices[indices[1 + 3 * idx] * 3 + 0], vertices[indices[1 + 3 * idx] * 3 + 1], vertices[indices[1 + 3 * idx] * 3 + 2] };
 		glm::vec3 v2 = { vertices[indices[2 + 3 * idx] * 3 + 0], vertices[indices[2 + 3 * idx] * 3 + 1], vertices[indices[2 + 3 * idx] * 3 + 2] };
-		glm::vec3 triangle[3] = { v0,v1,v2 };		
-		std::cout << "kreslim vertexy: v0: " << glm::to_string(triangle[0]) << " a v1: " << glm::to_string(triangle[1]) << " a v2: " << glm::to_string(triangle[2]) << " \n";
+		std::vector<glm::vec3> triangle;
+		triangle.push_back(v0);
+		triangle.push_back(v1);
+		triangle.push_back(v2);
+
 		return triangle;
 	}
 
-	static glm::vec3 getTriangleNormal(glm::vec3 vertices[3]){ return glm::normalize(glm::cross(vertices[1] - vertices[0], vertices[2] - vertices[0])); }
+	static glm::vec3 getTriangleNormal(std::vector<glm::vec3> vertices){ return glm::normalize(glm::cross(vertices[1] - vertices[0], vertices[2] - vertices[0])); }
 	static float getDistanceFromOrigin(glm::vec3 normal, glm::vec3 vertex) { return glm::dot(normal,vertex); }
-	static float getPlaneIntersectionDistance(float distance_from_origin,glm::vec3 plane_normal,glm::vec3 origin, glm::vec3 ray_direction, bool& _singleSided) {float ray_dot_normal = glm::dot(plane_normal, ray_direction);if (ray_dot_normal == 0 || (ray_dot_normal > 0 && _singleSided)/*ray&plane parallel + dont divide by zero*/) { return 0;}	else { return ((glm::dot(plane_normal, origin) + distance_from_origin) / ray_dot_normal);}
+	static float getPlaneIntersectionDistance(float distance_from_origin,glm::vec3 plane_normal,glm::vec3 origin, glm::vec3 ray_direction, bool& _singleSided) {	float ray_dot_normal = glm::dot(plane_normal, ray_direction);if (ray_dot_normal == 0 || (ray_dot_normal > 0 && _singleSided)/*ray&plane parallel + dont divide by zero*/) { return 0;}	else { return ((glm::dot(plane_normal, origin) + distance_from_origin) / ray_dot_normal);}
 	
 	}
 	static glm::vec3 getPlaneIntersection(glm::vec3 origin, float intersection_distance,glm::vec3 ray_direction) { return origin + (intersection_distance*ray_direction) ;}
 	///TODO
-	static bool rayHitTriangle(glm::vec3 triangle[3], glm::vec3 origin, glm::vec3 ray_dir, Camera camera, bool& singleSided);
+	static bool rayHitTriangle(std::vector<glm::vec3> triangle, glm::vec3 origin, glm::vec3 ray_dir, Camera camera, bool singleSided);
 
 	~RT_Mesh();
 
