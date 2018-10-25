04eb470c18f1d7d3a4abc8ddb65bd743e63a82ec
xskace12 <xskace12@vutbr.cz>
2018-10-25 17:15:45 +0200

    flip camera

--- Raytracer/Raytracer/Ray.cpp
+++ Raytracer/Raytracer/Ray.cpp
@@ -26,7 +26,7 @@ void Ray::calcRayPerspectiveDirection(Ray *ray, float x, float y,float w, float 
 {
 	//glm::vec3 ray_direction = glm::normalize(/*glm::vec4(camera.camera_position,w) -*/ camera.view_matrix*glm::vec4( glm::vec3(x,y,near),w));
 	glm::vec4 ray_origin = glm::inverse(camera.view_matrix)*glm::vec4(0.f, 0.f, 0.f,0.0f);
-	ray->direction = glm::normalize(ray_origin-(glm::inverse(camera.view_matrix)*glm::vec4(glm::vec3(x, y, near), w)));
+	ray->direction = glm::normalize((glm::inverse(camera.view_matrix)*glm::vec4(glm::vec3(x, y, near), w))-ray_origin);
 	ray->origin = ray_origin;
 	return;// ray_direction;
 }
