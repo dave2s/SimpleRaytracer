7befd6da66913007bb8e93e9b171aff089f6a0ef
xskace12 <xskace12@vutbr.cz>
2018-10-25 19:32:31 +0200

    Experiments with projection-not working correctly

--- Raytracer/Raytracer/Ray.cpp
+++ Raytracer/Raytracer/Ray.cpp
@@ -22,11 +22,12 @@ glm::vec3 Ray::calcRayDirection(glm::vec3 origin, glm::vec3 target) {
 	return glm::normalize(target - origin);
 }
 
-void Ray::calcRayPerspectiveDirection(Ray *ray, float x, float y,float w, float near,Camera camera)
+void Ray::calcRayPerspectiveDirection(Ray *ray, float x, float y,float w, float near,Camera &camera)
 {
 	//glm::vec3 ray_direction = glm::normalize(/*glm::vec4(camera.camera_position,w) -*/ camera.view_matrix*glm::vec4( glm::vec3(x,y,near),w));
-	glm::vec4 ray_origin = glm::inverse(camera.view_matrix)*glm::vec4(0.f, 0.f, 0.f,0.0f);
-	ray->direction = glm::normalize((glm::inverse(camera.view_matrix)*glm::vec4(glm::vec3(x, y, near), w))-ray_origin);
-	ray->origin = ray_origin;
+	glm::vec3 ray_origin = /*glm::inverse(camera.view_matrix)**/glm::vec3(0.f, 0.f, 0.f);
+	
+	ray->direction = glm::normalize(glm::vec3(x, y, -1.0));
+	ray->origin = glm::vec3(ray_origin);
 	return;// ray_direction;
 }
--- Raytracer/Raytracer/Ray.h
+++ Raytracer/Raytracer/Ray.h
@@ -12,6 +12,6 @@ public:
 	glm::vec3 direction;
 
 	static glm::vec3 calcRayDirection(glm::vec3 origin, glm::vec3 target);
-	static void calcRayPerspectiveDirection(Ray *ray,float x, float y, float w, float near, Camera camera);
+	static void calcRayPerspectiveDirection(Ray *ray,float x, float y, float w, float near, Camera &camera);
 };
 
