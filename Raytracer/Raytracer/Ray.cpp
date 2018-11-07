735442b68082b1136e62b868fdaea1deaec99b93
Dave2S <xskace12@vutbr.cz>
2018-11-07 13:23:07 +0100

    refactor tracing - encapsulated intoseparate method

--- Raytracer/Raytracer/Ray.cpp
+++ Raytracer/Raytracer/Ray.cpp
@@ -23,13 +23,10 @@ glm::vec3 Ray::calcRayDirection(glm::vec3 origin, glm::vec3 target) {
 	return glm::normalize(target - origin);
 }
 
-void Ray::calcRayPerspectiveDirection(Ray *ray, float x, float y,float w, float near,Camera &camera)
+void Ray::calcRayPerspectiveDirection(glm::vec3 &origin,glm::vec3 &dir, float x, float y,float w, float near,Camera &camera)
 {
-	//glm::vec3 ray_direction = glm::normalize(/*glm::vec4(camera.camera_position,w) -*/ camera.view_matrix*glm::vec4( glm::vec3(x,y,near),w));
-	glm::vec3 ray_origin = glm::inverse(camera.view_matrix)*glm::vec4(0.f,0.f,0.f,1.f);
-	//glm::vec3 ray_origin = camera.camera_position;
-	ray->direction = glm::normalize(glm::vec3(glm::inverse(camera.view_matrix)*glm::vec4(x, y, -near,1.f)) - ray_origin);
-	//ray->direction = glm::normalize(glm::inverse(camera.view_matrix)*glm::vec4(x, y, -near,1.0f)-ray_origin);
-	ray->origin = ray_origin;
-	return;// ray_direction;
+	origin = glm::inverse(camera.view_matrix)*glm::vec4(0.f, 0.f, 0.f, 1.f);
+	dir = glm::normalize(glm::vec3(glm::inverse(camera.view_matrix)*glm::vec4(x, y, -near, 1.f)) - origin);
+	return;
 }
+
--- Raytracer/Raytracer/Ray.h
+++ Raytracer/Raytracer/Ray.h
@@ -14,7 +14,7 @@ public:
 	float	prev_D; //if primary, set, if shadow - read
 
 	static glm::vec3 calcRayDirection(glm::vec3 origin, glm::vec3 target);
-	static void calcRayPerspectiveDirection(Ray *ray,float x, float y, float w, float near, Camera &camera);
+	static void calcRayPerspectiveDirection(glm::vec3 &origin, glm::vec3 &dir,float x, float y, float w, float near, Camera &camera);
 	static void calcReflectedDirection(glm::vec3 &NHit, glm::vec3 & raydir) { raydir = raydir - 2.f * NHit*(glm::dot(NHit, raydir)); }
 
 	static float norm(glm::vec3 vec) { return (vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]); }
