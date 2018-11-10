00ea76c98687bcdb4b046d366fb26379b3f30219
Dave2S <xskace12@vutbr.cz>
2018-11-10 14:16:09 +0100

    add models

--- Raytracer/Raytracer/ModelLoader.h
+++ Raytracer/Raytracer/ModelLoader.h
@@ -1,8 +1,20 @@
 #pragma once
+#include <string>
+#include <vector>
+
 class ModelLoader
 {
 public:
 	ModelLoader();
+
+	
+	struct Texture {
+		unsigned int id;
+		std::string path;
+		
+	};
+	std::vector<Texture> textures;
+
 	~ModelLoader();
 };
 
