a947fb9872b427ff006d95a2e0ea17f448428840
xskace12 <xskace12@vutbr.cz>
2018-12-31 19:02:19 +0100

    dokumentace

--- Raytracer/Raytracer/BVH.h
+++ Raytracer/Raytracer/BVH.h
@@ -4,11 +4,36 @@ https://www.highperformancegraphics.org/wp-content/uploads/2015/Papers-Session1/
 Bounding Volume Hierarchy Optimization
 through Agglomerative Treelet Restructuring
 
+Fast Parallel Construction of High-Quality Bounding Volume Hierarchies
+Tero Karras Timo Aila NVIDIA
+https://research.nvidia.com/publication/fast-parallel-construction-high-quality-bounding-volume-hierarchies
+
 */
+
+#include <cstdint>
+#include "GLM/glm.hpp"
 class BVH
 {
+
 public:
+
 	BVH();
 	~BVH();
+
+	//store further
+	struct SABounds {
+		SABounds() {
+			for (uint8_t i = 0; i < plane_count; ++i) {
+				//d[i][0] = inf;
+				//d[i][1] = -inf;
+			}
+		}
+	};
+
+private:
+	static const uint8_t plane_count = 7;
+
+	const glm::vec3 setPlanes[plane_count];
+	
 };
 
--- Raytracer/Raytracer/Defines.h
+++ Raytracer/Raytracer/Defines.h
@@ -9,7 +9,6 @@
 #endif
 
 #define PROFILING
-#define BBAccel
 //#define ONE_FRAME
 
 #define HIT_BIAS 0.0005f // to prevent shadow acne
@@ -25,21 +24,23 @@
 #endif
 
 #define TEXTURE_REPEAT
-//#define FINAL_RENDER
+#define FINAL_RENDER
 
 #ifndef FINAL_RENDER
 #define SCREEN_SPACE_SUBSAMPLE 1
-#define MSAA
-#define WIDTH 480
-#define HEIGHT 360
+#define BBAccel
+//#define MSAA
+#define WIDTH 1280
+#define HEIGHT 720
 #define MAX_DEPTH 3
 #define SMOOTH_SHADING
 #define PROFILE
 #else
 #define SCREEN_SPACE_SUBSAMPLE 1
-//#define MSAA
-#define WIDTH 1920
-#define HEIGHT 1080
+#define MSAA
+#define BBAccel
+#define WIDTH 1024
+#define HEIGHT 768
 #define MAX_DEPTH 3
 #define SMOOTH_SHADING
 #define PROFILE
@@ -49,13 +50,23 @@
 const std::vector<std::vector<float>> msaa_sample_coords/*(2, std::vector< float >(2, 0.f))*/{ {0.f,0.f}, {-0.25f,+0.25f}, {+0.25f,+0.25f},{-0.25f,-0.25f},{+0.25f,-0.25f} };
 #endif
 
-const float global_light_intensity = .8f;
+// mark functions returning via reference
+#define OUT
+
+// convert  <0.f,1.f> to <0,255>
+#define F2U(float_x) (uint8_t)round(float_x * 255.0f)
+#define F32vec2U8vec(float_vec) glm::u8vec3(F2U(float_vec[0]),F2U(float_vec[1]),F2U(float_vec[2]))
+#define U2F(uint_x) (float)uint_x/255.0f
+#define U8vec2F32vec(uint_vec) glm::f32vec3(U2F(uint_vec[0]),U2F(uint_vec[1]),U2F(uint_vec[2]))
+
+
+const float global_light_intensity = .7f;
 const glm::f32vec3 const_sky_color = glm::f32vec3(U2F(160), U2F(217), U2F(255));
 
-//const std::string DEFAULT_MODEL = "example/sponza/sponza.obj";
+//const std::string DEFAULT_MODEL = "example/sponza2/sponza.obj";
 //const std::string DEFAULT_MODEL = "example/CornellBox/CornellBox-Mirror.obj";
 //const std::string DEFAULT_MODEL = "example/bunny/bunny.obj";
-const std::string DEFAULT_MODEL = "example/f16/f16.obj";
-//const std::string DEFAULT_MODEL = "example/suzanne/suzanne.obj";
+//const std::string DEFAULT_MODEL = "example/f16/f16.obj";
+const std::string DEFAULT_MODEL = "example/suzanne/suzanne.obj";
 //const std::string DEFAULT_MODEL = "example/cruiser/cruiser.obj";
 //const std::string DEFAULT_MODEL = "example/armadillo/armadillo.ply";
